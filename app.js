926104d3947acf19409f5431d33f525e6b82e1a3
Pijush Chakraborty <piyushchakraborty9@gmail.com>
2019-11-30 12:43:46 +0530

    almost done

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require("express");
+var setUpPassport = require("./setuppassport");
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 var path = require("path");
+var passport = require("passport");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 var session = require("express-session");
@@ -8,6 +11,7 @@ var flash = require("connect-flash");
 var routes = require("./routes");
 var app = express();
 mongoose.connect("mongodb://localhost:27017/");
+setUpPassport();
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -18,8 +22,19 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(flash());
 app.use(routes);
 app.listen(app.get("port"), function () {
     console.log("Server started on port " + app.get("port"));
 });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+    secret: "TKRv0IJs=HYqrvagQ#&!F!%V]Ww/4KiVs$s,<<MX",
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
